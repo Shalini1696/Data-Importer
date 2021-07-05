c559f5ac7902c0cb1b5ea9b578b75a6ca76237bd
Shalini-Atos <shalini.t@atos.net>
2021-07-05 14:55:17 +0530

    new commit

---
+++ force-app/main/default/lwc/objectFieldSelection/objectFieldSelection.js
@@ -0,0 +1,106 @@
+import { LightningElement, wire, api, track } from 'lwc';
+import getObjectFields from '@salesforce/apex/SObjectFieldsInfo.getObjectFields';
+import getsObjectNames from '@salesforce/apex/SObjectFieldsInfo.getsObjectNames';
+import getRequiredFields from '@salesforce/apex/SObjectFieldsInfo.getRequiredFields';
+import getFieldType from '@salesforce/apex/SObjectFieldsInfo.getFieldType';
+
+export default class ObjectFieldSelection extends LightningElement {
+    selectedObject; //Selected Object from ComboBox
+    @api comboBoxLabelName = 'Select Object Name';
+    //@api isChild = false;
+    objNames; //Holds all Standard & Custom object Names
+    fieldNames = []; //Holds all the fields of an object
+
+    //lightning dual-list box variables
+    selected = [];
+    values = [];
+    requiredOptions = [];
+    
+    isSubmitClicked = false;
+    @track mapOfFieldTypes = [];
+
+    handleObjectNameChange(event) {
+        this.selectedObject = event.detail.value;
+    }
+
+    selectedOptionHandler(event) {
+        this.selected = event.detail.value; 
+    }
+
+    get selectedFields() {
+        return this.selected.length ? this.selected : 'none';
+    }
+
+    @wire(getsObjectNames)
+    wiredObjectNames({ data, error }) {
+        if (data) {
+            this.objNames = [{ value: '', label: 'All Objects' }];
+             data.forEach(element => {
+                const objName = {};
+                objName.label = element;
+                objName.value = element;
+                this.objNames.push(objName);
+            });
+        }
+    }
+
+    @wire(getObjectFields, { objectName: '$selectedObject' })
+    wiredObjectFields({ data, error }) {
+        if (data) {
+            const fnames = [];
+            data.forEach(element => {
+                const fieldName = {};
+                fieldName.label = element;
+                fieldName.value = element;
+                fnames.push(fieldName);
+            });
+            this.fieldNames = fnames.slice();
+        }
+    }
+
+    @wire(getRequiredFields, { objectName: '$selectedObject' })
+    wiredRequiredFields({ data, error }) {
+        if (data) {
+            const reqField = [];
+            data.forEach(element => {
+                reqField.push(element);
+            });
+            this.requiredOptions = reqField.slice();
+            this.values = this.requiredOptions;
+           }
+    }
+ 
+    @wire(getFieldType, { objectName: '$selectedObject', selectedFieldNames: '$selected'})
+    wiredFieldTypes({data, error}){
+        if(data){   
+            const fieldTypes = [];
+            for(let key in data) {
+                if (data.hasOwnProperty(key)) { 
+                    fieldTypes.push({value:data[key], key:key});
+                }
+            }
+            this.mapOfFieldTypes = fieldTypes.slice();
+        }
+        else if(error){
+            console.log(JSON.stringify(error));
+        }
+    }
+    
+    downloadTemplateHandler(event) {
+        this.disableNextButton1 = false;
+        let header = this.selected + "\n"; 
+        let csvFile = document.createElement('a');
+        csvFile.href = 'data:text/csv;charset=utf-8,' + encodeURI(header);
+        csvFile.target = '_blank';
+        csvFile.download = `${this.selectedObject}.csv`;
+        csvFile.click();
+    }
+
+    submitButtonHandler(event){
+        const selectedEvent = new CustomEvent('submit', {
+            detail:{selectedObj:this.selectedObject} 
+          });
+        this.dispatchEvent(selectedEvent);
+        this.isSubmitClicked = true;
+    }
+}
